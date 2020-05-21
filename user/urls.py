ee3ce12560a4b5d5ff9cbba1a8f29c19a4e381e4
김수형 <soohyung0121@gmail.com>
2020-05-21 19:57:06 +0900

    문자 인증 뷰 추가, xml 파일 정리 등

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import KakaoLogin 
+from .views import KakaoLogin, SignUpView, SignInView, Activate
 
 urlpatterns = [
         path('/kakao-login', KakaoLogin.as_view()),
+        path('/sign-up', SignUpView.as_view()),
+        path('/activate/<str:uidb64>/<str:token>', Activate.as_view()),
+        path('/sign-in', SignInView.as_view())
 ]
